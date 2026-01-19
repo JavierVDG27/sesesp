708579d03c64cb34c6e9c592660ddc9a999c42b6
MartinezMiguelAlexis <20161266@itoaxaca.edu.mx>
2026-01-19 03:56:41 -0600

    Auto migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Solo ejecutamos la migración si estamos en Render (producción)
+    if (config('app.env') === 'production') {
+        try {
+            Artisan::call('migrate', ['--force' => true]);
+        } catch (\Exception $e) {
+            // Si ya están creadas, no pasará nada
+        }
+    }
     }
 }
